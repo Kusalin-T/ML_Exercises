5731240adc8c397617e3cd7914adf61a6003f7b2
KusalinTH <kt1218@ic.ac.uk>
2024-06-20 13:47:39 +0700

    Basics 2_var

---
+++ Basics/Exercise/2_variables/2_vaiables_exercise.py
@@ -0,0 +1,16 @@
+#break=5
+
+birth_year = 2000
+current_year = 2024
+age = current_year - birth_year
+print("My age is", age)
+
+first = "Kusalin"
+second = "Thanyakulsajja"
+print(first+' '+second)
+
+'''
+1record
+record-one
+record^one
+'''
