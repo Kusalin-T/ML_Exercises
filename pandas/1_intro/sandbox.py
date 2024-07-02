ab69f862d3b5ef9395995a72257d1bab726e3414
KusalinTH <kt1218@ic.ac.uk>
2024-07-02 20:46:54 +0700

    Pandas 1-2

---
+++ pandas/1_intro/sandbox.py
@@ -0,0 +1,6 @@
+import pandas as pd
+df = pd.read_csv('pandas\\1_intro\\nyc_weather.csv')
+print(df['Temperature'].max())
+print(df['EST'][df['Events']=='Rain'])
+df.fillna(0, inplace=True)
+print(df['WindSpeedMPH'].mean())
